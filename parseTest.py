38d0602420036f638a474d545155f0d87c3a6ded
jyuan1322 <jy1322@rit.edu>
2015-04-01 02:47:22 -0400

    added a simplified parser - this isn't the full nested list, but it should be enough for classification
    it makes a dict at the symbol level, where each value is a list of dicts
    at the stroke level - in the inner dict, the key is the stroke id, the
    value is x,y,x,y,x,y as a string
    
    I got rid of the equation level for now and kept the coordinates as
    string to reduce by 2 levels, it's 3 nested levels now

--- parseTest.py
+++ parseTest.py
@@ -1,16 +1,65 @@
 import xml.etree.ElementTree as ET
 import os
+import pickle
+from collections import defaultdict
 
 path = 'C:\\Users\\Jie\\Desktop\\Misc_RIT_documents\\PatternRecognition\\TrainINKML_v3\\extension\\test'
 
+#{symbol: list of instances}
+#each instance is {1:[x,y,x,y], 2:[x,y,x,y], 5:[x,y,x,y]}
+#since the whole equation is lost, trace number is ordinal only
+symbolList = defaultdict(list)
+ns = {'inkml': 'http://www.w3.org/2003/InkML'}
 for filename in os.listdir(path):
 	fullname = os.path.join(path, filename)
 	tree = ET.parse(fullname)
 	root = tree.getroot()
 	
-	#for child in root:
-	#	print(child.tag)
-	for topTraceGroup in root.findall('{http://www.w3.org/2003/InkML}traceGroup'):
-		for subTraceGroup in topTraceGroup.findall('{http://www.w3.org/2003/InkML}traceGroup'):
-			print(subTraceGroup.tag)
+	#store trace strokes in list
+	'''traceList = []
+	for trace in root.findall('inkml:trace',ns):
+		traceIndex = int(trace.attrib['id'])
+		traceVals = trace.text.replace(',','').replace(' ',',')
+		traceValSplit = traceVals.split(',')
+		x=[]
+		y=[]
+		for i in range(0,len(traceValSplit)):
+			if i % 2 == 0:
+				x.append(int(traceValSplit[i]))
+			else:
+				y.append(int(traceValSplit[i]))
+		traceList.insert(traceIndex,[x, y])
+	print(traceList)'''
 	
+	
+	#parse traceGroups, one instance
+	'''for topTraceGroup in root.findall('inkml:traceGroup',ns):
+		for subTraceGroup in topTraceGroup.findall('inkml:traceGroup',ns):
+			subTraceGroupList = []
+			for traceStroke in subTraceGroup.findall('inkml:traceView',ns):
+				subTraceGroupList.append(int(traceStroke.attrib['traceDataRef']))
+			traceGroupList.append(subTraceGroupList)
+	print(traceGroupList)'''
+	
+	
+	#extract individual symbols
+	for topTraceGroup in root.findall('inkml:traceGroup',ns):
+		for subTraceGroup in topTraceGroup.findall('inkml:traceGroup',ns):
+			truthSymbol = "" #will store the true annotation
+			symbolTraces = {}
+			for annotation in subTraceGroup.findall('inkml:annotation',ns):
+				truthSymbol = annotation.text
+			for traceView in subTraceGroup.findall('inkml:traceView',ns):
+				traceNum = int(traceView.attrib['traceDataRef'])
+				for trace in root.findall('inkml:trace',ns):
+					traceId = int(trace.attrib['id'])
+					if traceId == traceNum:
+						symbolTraces[traceId] = trace.text.replace('\n','').replace(',','').replace(' ',',')
+			symbolList[truthSymbol].append(symbolTraces)
+			
+#pickle.dump(symbolList, open("symbolList.p","wb"))
+for a in symbolList:
+	print(a)
+	print(symbolList[a])
+	print('')
+	print('')
