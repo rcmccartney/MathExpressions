404689f89d2954df078a1c3c033bb95f1e22aab0
Rob <rm7536@rit.edu>
2015-04-02 00:49:37 -0400

    fixed bad merge

--- parseTestClasses.py
+++ parseTestClasses.py
@@ -5,13 +5,7 @@ from collections import defaultdict
 
 path = 'C:\\Users\\Jie\\Desktop\\Misc_RIT_documents\\PatternRecognition\\TrainINKML_v3\\extension\\test'
 
-<<<<<<< HEAD
 
-class Trace():
-
-    def __init__(self, id, pointList):
-        self.id = id
-=======
 class Trace:
     def __init__(self, id, pointList):
         self.id = id
@@ -20,34 +14,20 @@ class Trace:
         traceValList = []
         for i in range(0,len(xy),2):
             self.points.append((int(xy[i]),int(xy[i+1])))'''
->>>>>>> origin/master
         xy = pointList.split(',')
         self.x = xy[::2]
         self.y = xy[1::2]
 
-<<<<<<< HEAD
-
-class Symbol():
-
-    def __init__(self, label, traceList):
-        self.label = label
-        self.traceList = traceList
-
-
-class InkmlFile():
-
-=======
 class Symbol:
     def __init__(self, label, traceList):
         self.label = label
         self.traceList = traceList
         
 class InkmlFile:
->>>>>>> origin/master
+
     def __init__(self, label, symbolList):
         self.label = label
         self.symbolList = symbolList
-
 
 ns = {'inkml': 'http://www.w3.org/2003/InkML'}
 inkmlFileList = []
@@ -55,31 +35,7 @@ for filename in os.listdir(path):
     fullname = os.path.join(path, filename)
     tree = ET.parse(fullname)
     root = tree.getroot()
-<<<<<<< HEAD
 
-    # get all traces for eqn
-    traceListTemp = {}
-    for trace in root.findall('inkml:trace', ns):
-        traceId = int(trace.attrib['id'])
-        #generate list of (x,y)
-        traceVal = trace.text.replace('\n', '').replace(',', '').replace(' ', ',')
-        traceListTemp[traceId] = Trace(traceId, traceVal)
-
-    #get all symbols
-    symbolList = []
-    topTraceGroup = root.find('inkml:traceGroup', ns)
-    for subTraceGroup in topTraceGroup.findall('inkml:traceGroup', ns):  #for each symbol
-        annotation = subTraceGroup.find('inkml:annotation', ns).text
-        #get traceList for symbol
-        traceViewListTemp = []
-        for traceView in subTraceGroup.findall('inkml:traceView', ns):
-            id = int(traceView.attrib['traceDataRef'])
-            traceViewListTemp.append(id)
-        traceList = [traceListTemp[k] for k in traceViewListTemp]  #trace subset for symbol
-
-        symbolList.append(Symbol(annotation, traceList))
-=======
-    
     #get all traces for eqn
     traceListTemp = {}
     for trace in root.findall('inkml:trace',ns):
@@ -107,13 +63,7 @@ for filename in os.listdir(path):
         if annotation.attrib['type'] == 'truth':
             label = annotation.text
     inkmlFileList.append(InkmlFile(label,symbolList))
->>>>>>> origin/master
 
-    #generate class for equation
-    for annotation in root.findall('inkml:annotation', ns):
-        if annotation.attrib['type'] == 'truth':
-            label = annotation.text
-    inkmlFileList.append(InkmlFile(label, symbolList))
 
 for file in inkmlFileList:
     print(file.label)
