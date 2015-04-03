fde91bf08617d84301c9422196e2dcb766ae4c67
Rob <rm7536@rit.edu>
2015-04-02 22:02:10 -0400

    created feature extraction class
    empty placeholder for now

---
+++ features.py
@@ -0,0 +1,8 @@
+__author__ = 'mccar_000'
+
+class FeatureExtraction():
+    """
+    Takes in a train and test set of Inkml and produces
+    a 2D array of features and a 1D array of output classes
+    """
+    
