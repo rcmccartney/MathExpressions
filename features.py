a19264da61f642aefe10b115ea1bc26bfb332d21
Rob <rm7536@rit.edu>
2015-04-02 22:05:42 -0400

    added fake data to test flow of system

--- features.py
+++ features.py
@@ -5,4 +5,17 @@ class FeatureExtraction():
     Takes in a train and test set of Inkml and produces
     a 2D array of features and a 1D array of output classes
     """
-    
+
+    def __init__(self, train, test):
+        self.train = train
+        self.test = test
+        self.x = []  # 2d array of features
+        self.y = []  # 1d array of class values
+
+    def get_fake_data(self):
+        """
+        used to test the flow of the system
+        """
+        x = [[1, 2, 3, 4], [10, 9, 8, 7]]
+        y = [0, 1]
+        return x,y
