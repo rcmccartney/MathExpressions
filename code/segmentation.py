406930c2bf7f703d2d13571c00ceda235eaa1b4f
jyuan1322 <jy1322@rit.edu>
2015-04-26 21:01:42 -0400

    suppress printing from dp

--- code/segmentation.py
+++ code/segmentation.py
@@ -31,23 +31,23 @@ class Segmenter():
                 temp_symbol = Symbol(None, None, None, None, [trace_list[0]])
                 feature_set = feature_extractor.get_single_feature_set(temp_symbol,0)
                 maxkey, maxdist = classifier.eval(feature_set, 1)
-                print("maxkey", maxkey)
-                print("maxdist", maxdist)
+                #print("maxkey", maxkey)
+                #print("maxdist", maxdist)
                 best.append(maxdist)  # index 0
                 backtrack.append(-1)  # indicates start of array
                 bestclass.append(maxkey)
                 for i in range(1, len(trace_list)):
-                    print("-----")
+                    #print("-----")
                     best.append(float("-inf"))
                     backtrack.append(-1)
                     bestclass.append("temp")
                     for j in range(i-1, -1, -1):
                         subset = trace_list[j+1:i+1]
-                        print("subset: ", j+1, ", ", i+1)
+                        #print("subset: ", j+1, ", ", i+1)
                         temp_symbol = Symbol(None, None, None, None, subset)
                         feature_set = feature_extractor.get_single_feature_set(temp_symbol,0)
                         maxkey, maxdist = classifier.eval(feature_set, len(subset))
-                        print("maxkey: ", maxkey, "maxdist: ", maxdist)
+                        #print("maxkey: ", maxkey, "maxdist: ", maxdist)
                         dist = best[j] + maxdist
                         if dist > best[i]:
                             best[i] = dist
@@ -61,12 +61,13 @@ class Segmenter():
                         best[i] = maxdist
                         backtrack[i] = -1
                         bestclass[i] = maxkey
-                print(best)
-                print(backtrack)
-                print(bestclass)
-                print("")
-                print("")
-                print("")
+                #print(best)
+                #print(backtrack)
+                #print(bestclass)
+                #print("")
+                #print("")
+                #print("")
+                
                 best_list.append(best)
                 backtrack_list.append(backtrack)
                 bestclass_list.append(bestclass)
